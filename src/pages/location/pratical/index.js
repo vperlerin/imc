b1a1182e6e34fcacb09745086a3ee7cd60de8fd9
vperlerin <vperlerin@nerd.com>
2025-02-03 17:36:28 +0100

    New pages

--- src/pages/location/pratical/index.js
+++ src/pages/location/pratical/index.js
@@ -1,10 +1,40 @@
 import PageContain from "components/page-contain";
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Pratical = () => {
   return (
     <PageContain title="Pratical Information">
-      
+      <h2>Weather</h2>
+      <p>
+        The Netherlands has a temperate maritime climate influenced by the North Sea and Atlantic Ocean, with cool summers and moderate winters. In September temperatures average around 15°C, but can reach 19.5 °C during daytime, and 10 °C at night. While September can bring sunny spells, rainfall is always a possibility so be sure to pack some rain gear. Please check the weather forecast before you travel. If you plan to join the sightseeing tour on Saturday afternoon, be sure to bring a pair of comfortable walking shoes.
+      </p>
+
+
+      <h2>Visas</h2>
+      <p>
+        If you are coming to the Netherlands you should always check if you need a visa and, if so, what type of visa. Find out how to apply for a visa and how to track the status of your application via the website of the <a href="https://www.netherlandsworldwide.nl/visa-the-netherlands" rel="noopener noreferrer">Dutch Government</a>.
+      </p>
+
+      <p>
+        If you require an invitation letter for a visa, please provide your legal private residence or business address, your passport number, and the address of the Dutch consulate or Embassy where your visa application will be submitted. Please note that the LOC reserves the right to refuse to issue an invitation letter if there is any doubt about the purpose of your visit.
+      </p>
+
+      <h2>Currency</h2>
+      <p>
+        The local currency in the Netherlands is the Euro (€). You can exchange foreign currencies to Euro in the main cities or withdraw some cash at the main train stations or at the airports. At Utrecht Central station you can also find the GWK Travelex. <b>Note that Stayokay Soest is a cashless hostel. You can pay with debit or credit card, but they do not accept cash money.</b></p>
+
+      <h2>Power Sockets</h2>
+      <p>The sockets are type F with a voltage of 230 V and a frequency of 50 Hz (standard Euro plug). Please take a travel adapter with you to ensure that your plug fits.</p>
+
+      <p>The LOC can usually also provide assistance with matters not covered on these pages. They can be contacted via the  {' '}
+        <Link
+        aria-label="Contact"
+        to="/contact"
+        title="Contact"
+      >
+        contact page
+      </Link>.</p>
     </PageContain>
   );
 };
--- src/pages/location/travel/index.js
+++ src/pages/location/travel/index.js
@@ -4,9 +4,41 @@ import React from "react";
 const Travel = () => {
   return (
     <PageContain title="Travel Information">
-      
+      <h2>How to travel to Soest</h2>
+      <p>he location of the IMC 2025 will be the small city of Soest, situated 10 kilometers away from Utrecht: the fourth largest city of the country, located in the centre of the Netherlands. Soest is easily reachable by road, train (thanks to the major railway hub Utrecht Central) and airplane (via Schiphol Airport and Rotterdam-The Hague Airport).</p>
+
+
+      <h3>By airplane</h3>
+      <p>
+        If you plan on travelling by airplane you can opt to fly to <a href="https://www.schiphol.nl/en/" target="_blank" rel="noopener noreferrer">Schiphol Airport</a> or <a href="https://www.rotterdamthehagueairport.nl/en/" target="_blank" rel="noopener noreferrer">Rotterdam-The Hague Airport</a> , which both offer various connections to European and global destinations. From there, either rent a car or continue travelling by train.</p>
+
+      <h3>By Train</h3>
+      <p>
+        Plan your international train travel to <a href="https://www.ns.nl/en/station-information/ut/utrecht-centraal" target="_blank" rel="noopener noreferrer"> Utrecht Central station</a>. If you are taking the train from one of the airports, you can travel in just 30 minutes from Schiphol to the Utrecht Central train station. The trip will be a bit longer from Rotterdam-The Hague airport; take the bus and subway to Rotterdam Central station and then take a train to Utrecht Central. From there, you can reach the hostel from the Soest-Zuid train station. Once you arrive, take a left across the tracks to the Bosstraat. Walk 600 meters and you will have arrived at the hostel.</p>
+
+
+      <h3>By Car</h3>
+      <p>
+        As Soest is located near the city of Utrecht, it is readily accessible by car via the A2, A12, A27, and A28 highways. Amsterdam is just 30 minutes away, and Brussels and Cologne are a two-hour drive.
+      </p>
+
+
+      <p>From the direction of the city of Utrecht (the A28), take exit 4 to Soest. You will pass an industrial area. Cross the railway line and turn left 200m after the traffic lights (Pimpelmees). After 100m follow Bosstraat (bend to the right). You will see the hostel on your right after about 400m.
+      </p>
+
+      <p>From the direction of the city Amersfoort you can reach Soest via several routes. In Soest you drive in the direction of the industrial area. Cross the tracks and turn left 50 metres after the traffic lights. Then follow the road to the right; the hostel is on your right.
+      </p>
+
+      <p>From the direction of the city Amsterdam/Hilversum (A1) or Hilversum (A27), take exit 10 to Soest. Turn right at Paleis Soestdijk (Soestdijk Palace). Turn left at the first set of traffic lights. Follow the road all the way (2 km) until it bends at a right angle. After 200m take the first street on the right (de Pimpelmees). This street changes into the Bosstraat. The hostel is on your right after 400m. </p>
+
+
+      <h4>Parking</h4>
+      <p>You can park for free in front of the hostel. Charging of your electric car is possible at one of the charging stations at the parking area.</p>
+
     </PageContain>
   );
 };
+
+
 
 export default Travel;
