5214e5513472abfa28265c7ad043fe291da1d627
vperlerin <vperlerin@nerd.com>
2025-02-05 19:12:34 +0100

    m

---
+++ api/config.php
@@ -0,0 +1,17 @@
+<?php
+// Load environment variables from /DATA/sites/imc2025.imo.net/env/.env
+$envPath = realpath(__DIR__ . '/../env/.env');   
+
+if (file_exists($envPath)) {
+    $lines = file($envPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) continue;  
+        list($key, $value) = explode('=', $line, 2);
+        putenv("$key=$value");
+        $_ENV[$key] = $value;
+        $_SERVER[$key] = $value;
+    }
+}
+ 
+?>
+ 
