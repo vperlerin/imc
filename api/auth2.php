45055c0977d9e203627048c0d6392926ea9179f1
vperlerin <vperlerin@nerd.com>
2025-02-13 11:24:37 +0100

    m

---
+++ api/auth2.php
@@ -0,0 +1,35 @@
+<?php
+
+use League\OAuth2\Client\Provider\Google;
+
+require '../vendor/autoload.php';
+require_once "config.php";
+
+$provider = new Google([
+  'clientId'     => getenv("GOOGLE_CLIENT_ID"),
+  'clientSecret' => getenv("GOOGLE_CLIENT_SECRET"),
+  'redirectUri'  => 'https://imc2025.imo.net',
+  'accessType' => 'offline',
+]);
+if (!empty($_GET['error'])) {
+  // Got an error; probably user denied access
+  exit('Got error: ' . htmlspecialchars($_GET['error'], ENT_QUOTES, 'UTF-8'));
+} elseif (empty($_GET['code'])) {
+  // If we don't have an authorization code, then get one
+  $authUrl = $provider->getAuthorizationUrl([
+    'scope' => [
+      'https://mail.google.com/'
+    ]
+  ]);
+  header('Location: ' . $authUrl);
+  exit;
+} else {
+  // Try to get an access token (using the authorization code grant)
+  $token = $provider->getAccessToken('authorization_code', [
+    'code' => $_GET['code']
+  ]);
+  // Use this to get a new access token if the old one expires
+  $refreshToken = $token->getRefreshToken();
+
+  echo ($refreshToken);
+}
