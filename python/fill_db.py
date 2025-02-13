67ac174b8c599f4d810a6e1e2b71eff6485ac16a
vperlerin <vperlerin@nerd.com>
2025-02-13 17:32:11 +0100

    m

--- python/fill_db.py
+++ python/fill_db.py
@@ -13,6 +13,7 @@ SQL_PATH = os.path.join(BASE_DIR, "python", "insert_data.sql")
 with open(JSON_PATH, "r") as file:   
     data = json.load(file)
 
+ 
 # Extract workshops
 workshop_inserts = [
     "INSERT INTO workshops (title, price) VALUES ('{title}', {cost});".format(
@@ -29,22 +30,11 @@ registration_inserts = [
     for room in data["costs"]["rooms"]
 ]
 
-# Extract T-shirt sizes & pricing
-tshirt_price = data["costs"]["tshirts"]["price"]
-tshirt_sizes = [
-    "INSERT INTO extra_options (participant_id, buy_tshirt, tshirt_size, tshirt_price) VALUES "
-    "(@participant_id, TRUE, '{size}', {price});".format(
-        size="{} {}".format(model.capitalize(), size), price=tshirt_price
-    )
-    for model in data["costs"]["tshirts"]["models"]
-    for size in data["costs"]["tshirts"]["sizes"]
-]
-
 # Combine all SQL statements
-sql_script = "\n".join(workshop_inserts + registration_inserts + tshirt_sizes)
+sql_script = "\n".join(workshop_inserts + registration_inserts)
 
 # Save to file
-with open(SQL_PATH, "w", encoding="utf-8") as sql_file:
+with open(SQL_PATH, "w") as sql_file:
     sql_file.write(sql_script)
 
 print("SQL script generated successfully: {}".format(SQL_PATH))
