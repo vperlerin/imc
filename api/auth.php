ceb7ea652b42ae54c630c20fcc4c610104a38bd4
vperlerin <vperlerin@nerd.com>
2025-02-13 10:39:50 +0100

    m

--- api/auth.php
+++ api/auth.php
@@ -1,13 +1,15 @@
 <?php
 
-session_start();
-var_dump($_SESSION);
-
-declare(strict_types=1);
-
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+
+declare(strict_types=1);
+
+session_start();
+var_dump($_SESSION);
+
+
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
