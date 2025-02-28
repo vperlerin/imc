f16c42ccbe07f3cf323b3b8cf30818e68aa4fff7
vperlerin <vperlerin@nerd.com>
2025-02-28 11:02:24 +0100

    m

--- php/auth3.php
+++ php/auth3.php
@@ -1,68 +1,99 @@
 <?php
-// Import PHPMailer classes into the global namespace
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\OAuth;
 
-// Import Google OAuth 2.0 provider
-use League\OAuth2\Client\Provider\Google;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// Start the session
 session_start();
 
-// Load Composer's autoloader
 require '../vendor/autoload.php'; 
 require_once "config.php";
 
 
-// Gmail API credentials
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\OAuth;
+use League\OAuth2\Client\Provider\Google;
+
+if (!isset($_GET['code']) && !isset($_GET['provider'])) {
+    ?>
+<html>
+<body>Select Provider:<br>
+<a href='?provider=Google'>Google</a><br>
+</body>
+</html>
+    <?php
+    exit;
+}
+
+ 
+
+$providerName = '';
+
+if (array_key_exists('provider', $_GET)) {
+    $providerName = $_GET['provider'];
+    $_SESSION['provider'] = $providerName;
+} elseif (array_key_exists('provider', $_SESSION)) {
+    $providerName = $_SESSION['provider'];
+}
+if (!in_array($providerName, ['Google', 'Microsoft', 'Yahoo'])) {
+    exit('Only Google, Microsoft and Yahoo OAuth2 providers are currently supported in this script.');
+}
+
+//These details are obtained by setting up an app in the Google developer console,
+//or whichever provider you're using.
 $clientId = getenv("SMTP_CLIENT_ID");
 $clientSecret = getenv("SMTP_CLIENT_SECRET");
 $redirectUri = 'https://imc2025.imo.net/php/auth3.php';
 
-// Create a new Google OAuth 2.0 provider
-$provider = new Google([
-    'clientId'     => $clientId,
-    'clientSecret' => $clientSecret,
-    'redirectUri'  => $redirectUri,
-    'accessType' => 'offline'
-]);
 
-// If we don't have an authorization code, then get one
+
+$params = [
+    'clientId' => $clientId,
+    'clientSecret' => $clientSecret,
+    'redirectUri' => $redirectUri,
+    'accessType' => 'offline'
+];
+
+$options = [];
+$provider = null;
+
+switch ($providerName) {
+    case 'Google':
+        $provider = new Google($params);
+        $options = [
+            'scope' => [
+                'https://mail.google.com/'
+            ]
+        ];
+        break;
+}
+
+if (null === $provider) {
+    exit('Provider missing');
+}
+
 if (!isset($_GET['code'])) {
-    // Get authorization URL
-    $authUrl = $provider->getAuthorizationUrl([
-        'scope' => [ 'https://mail.google.com/' ]
-    ]);
-    // Get the generated state and store it in session
+    //If we don't have an authorization code then get one
+    $authUrl = $provider->getAuthorizationUrl($options);
     $_SESSION['oauth2state'] = $provider->getState();
-    // Redirect the user to Google's OAuth consent page
     header('Location: ' . $authUrl);
     exit;
+    //Check given state against previously stored one to mitigate CSRF attack
+} elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
+    unset($_SESSION['oauth2state']);
+    unset($_SESSION['provider']);
+    exit('Invalid state');
+} else {
+    unset($_SESSION['provider']);
+    //Try to get an access token (using the authorization code grant)
+    $token = $provider->getAccessToken(
+        'authorization_code',
+        [
+            'code' => $_GET['code']
+        ]
+    );
+    //Use this to interact with an API on the users behalf
+    //Use this to get a new access token if the old one expires
+    echo 'Refresh Token: ', $token->getRefreshToken();
 }
-
-// Check if the authorization code exists
-if (isset($_GET['code']) && !empty($_GET['code'])) {
-    try {
-        // Verify the state to protect against CSRF attacks
-        if (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
-            unset($_SESSION['oauth2state']);
-            exit('Invalid state!');
-        }
-
-        // Get the access token with the refresh token
-        $token = $provider->getAccessToken('authorization_code', [
-            'code' => $_GET['code'],
-        ]);
-
-        // Retrieve the refresh token
-        $refreshToken = $token->getRefreshToken();
-
-    } catch (Exception $e) {
-        exit('Failed to get access token: ' . $e->getMessage());
-    }
-
-    // Add PHPMailer code here
-  
-}
-?>
