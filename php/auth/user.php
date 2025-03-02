6821c4309843dd942fed03c401b06271fc5f6867
vperlerin <vperlerin@nerd.com>
2025-03-02 12:22:28 +0100

    m

--- php/auth/user.php
+++ php/auth/user.php
@@ -18,7 +18,7 @@ header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
 session_start();
-require_once __DIR__ . "../config.php"; 
+require_once __DIR__ . "/../config.php"; 
 
 // Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
