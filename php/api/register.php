63b26ae5f127a701c323ce20a2ceeed3e2bdeffb
vperlerin <vperlerin@nerd.com>
2025-03-03 11:25:39 +0100

    m

--- php/api/register.php
+++ php/api/register.php
@@ -16,7 +16,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
  
-require_once __DIR__ . "../config.php";
+require_once __DIR__ . "/../config.php";
 require_once __DIR__ . "/class/Connect.class.php"; // Ensures $pdo is loaded
 require_once __DIR__ . "*/class/ConferenceData.class.php";
 require_once __DIR__ . "/class/Mail.class.php";
