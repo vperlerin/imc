9f769d438cce86519162970a2aeec17ae6218074
vperlerin <vperlerin@nerd.com>
2025-03-03 11:23:34 +0100

    m

--- php/api/register.php
+++ php/api/register.php
@@ -5,11 +5,16 @@ $allowed_origins = [
 ];
 
 if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+    header("Access-Control-Allow-Credentials: true");
     header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
 }
-header("Access-Control-Allow-Credentials: true");
+
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
  
 require_once __DIR__ . "../config.php";
 require_once __DIR__ . "../class/Connect.class.php"; // Ensures $pdo is loaded
