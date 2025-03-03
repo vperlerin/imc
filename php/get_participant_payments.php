fec2e22f2454ae9c0c021a018bf3b02928ded0e5
vperlerin <vperlerin@nerd.com>
2025-03-03 08:41:13 +0100

    m

--- php/get_participant_payments.php
+++ php/get_participant_payments.php
@@ -3,6 +3,9 @@ require '../vendor/autoload.php';
 require_once __DIR__ . "/config.php";
 require_once __DIR__ . "/class/PaymentManager.php";
  
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $allowed_origins = [
     "https://imc2025.imo.net",
