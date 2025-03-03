88d3463d74b345dd07a990c3662a29e94c14a4cd
vperlerin <vperlerin@nerd.com>
2025-03-03 10:38:12 +0100

    mp

--- php/admin/api/delete_participant.php
+++ php/admin/api/delete_participant.php
@@ -14,7 +14,7 @@ $allowed_origins = [
 if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
     header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
 }
- 
+header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: POST, OPTIONS, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
