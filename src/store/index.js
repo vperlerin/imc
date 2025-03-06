a3b265426939b46ddf65f349d9c06aa1ec6c8aac
vperlerin <vperlerin@nerd.com>
2025-03-06 20:36:24 +0100

    m

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth";
 
+ 
+const isJSON = (jsonString) => {
+  try {
+    const parsed = JSON.parse(jsonString);
+    return parsed && typeof parsed === "object" ? parsed : false;
+  } catch (e) {
+    return false;
+  }
+};
+
+ 
+const session = localStorage.getItem("session");
+const parsedSession = isJSON(session);
+
+ 
 const store = configureStore({
   reducer: {
     auth: authReducer,
   },
   preloadedState: {
-    auth: JSON.parse(localStorage.getItem("session"))
-      ? { isAuthenticated: true }
+    auth: parsedSession
+      ? { isAuthenticated: !!parsedSession.isAuthenticated }  
       : { isAuthenticated: false },
   },
 });
