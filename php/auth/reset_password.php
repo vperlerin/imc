e9542253f5907242aeec23b9a4eb51919e35b06d
vperlerin <vperlerin@nerd.com>
2025-03-06 16:31:38 +0100

    m

--- php/auth/reset_password.php
+++ php/auth/reset_password.php
@@ -1,6 +1,9 @@
 <?php 
 require_once __DIR__ . "/../class/Connect.class.php"; 
  
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $data = json_decode(file_get_contents("php://input"), true);
 $token = isset($data["token"]) ? trim($data["token"]) : "";
