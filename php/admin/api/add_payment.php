5da3603dced6de52667f1081c5c672d258940ab9
vperlerin <vperlerin@nerd.com>
2025-03-09 10:02:05 +0100

    m

--- php/admin/api/add_payment.php
+++ php/admin/api/add_payment.php
@@ -3,7 +3,6 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-require '../vendor/autoload.php';
 require_once __DIR__ . "/../../config.php";
 require_once __DIR__ . "/../../PaymentManager.php";
 require_once __DIR__ . "/../../class/Connect.class.php";
