7d421b565b731e916995af538ba4975bfbef2d91
vperlerin <vperlerin@nerd.com>
2025-03-09 10:03:24 +0100

    m

--- php/admin/api/add_payment.php
+++ php/admin/api/add_payment.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 require_once __DIR__ . "/../../config.php";
-require_once __DIR__ . "/../../class/PaymentManager.php";
+require_once __DIR__ . "/../../class/Payment.class.php";
 require_once __DIR__ . "/../../class/Connect.class.php";
  
 header("Content-Type: application/json");
