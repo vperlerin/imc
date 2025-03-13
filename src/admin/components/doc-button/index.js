b034cf82240b6cc41b6a1f1f3bc70168e5517659
vperlerin <vperlerin@nerd.com>
2025-03-13 10:51:09 +0100

    Fix doc links

--- src/admin/components/doc-button/index.js
+++ src/admin/components/doc-button/index.js
@@ -3,22 +3,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const formatIcons = {
-  xls: <FaFileExcel />, 
+  xls: <FaFileExcel />,
 };
 
 const DocButton = ({ 
   link, 
-  className,
+  className, 
   format = "xls", 
-  title = "Download" }) => {
+  title = "Download" 
+}) => {
+  const isPhpRoute = link.startsWith("/php/");
+
   return (
-    <Link
-      className={`btn btn-outline-success d-inline-flex align-items-center gap-2 ${className}`}
-      to={link}
-      aria-label={`Download ${title} in ${format.toUpperCase()} format`}
-    >
-      {formatIcons[format] || null} {title}
-    </Link>
+    isPhpRoute ? (
+      <a
+        href={link} 
+        className={`btn btn-outline-success d-inline-flex align-items-center gap-2 ${className}`}
+        aria-label={`Download ${title} in ${format.toUpperCase()} format`}
+      >
+        {formatIcons[format] || null} {title}
+      </a>
+    ) : (
+      <Link
+        className={`btn btn-outline-success d-inline-flex align-items-center gap-2 ${className}`}
+        to={link}
+        aria-label={`Download ${title} in ${format.toUpperCase()} format`}
+      >
+        {formatIcons[format] || null} {title}
+      </Link>
+    )
   );
 };
 
