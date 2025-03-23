63bc1e83a2d0863bff5231db0d40144fa284ece4
vperlerin <vperlerin@nerd.com>
2025-03-23 14:16:08 +0100

    m

--- php/auth/forgot_password.php
+++ php/auth/forgot_password.php
@@ -8,6 +8,12 @@ if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed
     header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
 }
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+require_once __DIR__ . "/../class/Connect.class.php"; 
+
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
