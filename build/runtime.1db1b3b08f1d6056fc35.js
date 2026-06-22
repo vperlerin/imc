362582fdbb0ef8fa2f41245ee0a07b774add3793
vperlerin <vperlerin@nerd.com>
2026-06-22 10:53:18 +0200

    travel

---
+++ build/runtime.1db1b3b08f1d6056fc35.js
@@ -0,0 +1,327 @@
+/******/(()=>{// webpackBootstrap
+/******/"use strict";
+/******/var e,a,f,d,t,r={},c={};
+/************************************************************************/
+/******/ // The module cache
+/******/
+/******/
+/******/ // The require function
+/******/function b(e){
+/******/ // Check if module is in cache
+/******/var a=c[e];
+/******/if(void 0!==a)
+/******/return a.exports;
+/******/
+/******/ // Create a new module (and put it into the cache)
+/******/var f=c[e]={
+/******/ // no module.id needed
+/******/ // no module.loaded needed
+/******/exports:{}
+/******/};
+/******/
+/******/ // Execute the module function
+/******/
+/******/
+/******/ // Return the exports of the module
+/******/return r[e](f,f.exports,b),f.exports;
+/******/}
+/******/
+/******/ // expose the modules object (__webpack_modules__)
+/******/b.m=r,
+/******/
+/************************************************************************/
+/******/ /* webpack/runtime/chunk loaded */
+/******/e=[],
+/******/b.O=(a,f,d,t)=>{
+/******/if(!f){
+/******/var r=1/0;
+/******/for(i=0;i<e.length;i++){
+/******/for(
+/******/var[f,d,t]=e[i],c=!0,n=0
+/******/;n<f.length;n++)
+/******/(!1&t||r>=t)&&Object.keys(b.O).every((e=>b.O[e](f[n])))?
+/******/f.splice(n--,1):(
+/******/c=!1,
+/******/t<r&&(r=t)
+/******/)
+/******/;
+/******/if(c){
+/******/e.splice(i--,1)
+/******/;var o=d();
+/******/void 0!==o&&(a=o)
+/******/}
+/******/}
+/******/return a;
+/******/}
+/******/t=t||0;
+/******/for(var i=e.length;i>0&&e[i-1][2]>t;i--)e[i]=e[i-1];
+/******/e[i]=[f,d,t]},
+/******/ // getDefaultExport function for compatibility with non-harmony modules
+/******/b.n=e=>{
+/******/var a=e&&e.__esModule?
+/******/()=>e.default
+/******/:()=>e
+/******/;
+/******/return b.d(a,{a}),a;
+/******/},
+/******/
+/******/ /* webpack/runtime/create fake namespace object */
+/******/f=Object.getPrototypeOf?e=>Object.getPrototypeOf(e):e=>e.__proto__
+/******/,
+/******/ // create a fake namespace object
+/******/ // mode & 1: value is a module id, require it
+/******/ // mode & 2: merge all properties of value into the ns
+/******/ // mode & 4: return value when already ns object
+/******/ // mode & 16: return value when it's Promise-like
+/******/ // mode & 8|1: behave like require
+/******/b.t=function(e,d){
+/******/if(
+/******/1&d&&(e=this(e)),8&d)return e;
+/******/if("object"==typeof e&&e){
+/******/if(4&d&&e.__esModule)return e;
+/******/if(16&d&&"function"==typeof e.then)return e;
+/******/}
+/******/var t=Object.create(null);
+/******/b.r(t);
+/******/var r={};
+/******/a=a||[null,f({}),f([]),f(f)];
+/******/for(var c=2&d&&e;"object"==typeof c&&!~a.indexOf(c);c=f(c))
+/******/Object.getOwnPropertyNames(c).forEach((a=>r[a]=()=>e[a]));
+/******/
+/******/
+/******/return r.default=()=>e
+/******/,b.d(t,r),t;
+/******/},
+/******/ // define getter functions for harmony exports
+/******/b.d=(e,a)=>{
+/******/for(var f in a)
+/******/b.o(a,f)&&!b.o(e,f)&&
+/******/Object.defineProperty(e,f,{enumerable:!0,get:a[f]})
+/******/;
+/******/},
+/******/b.f={},
+/******/ // This file contains only the entry chunk.
+/******/ // The chunk loading function for additional chunks
+/******/b.e=e=>Promise.all(Object.keys(b.f).reduce(((a,f)=>(
+/******/b.f[f](e,a),a)
+/******/),[]))
+/******/,
+/******/ // This function allow to reference async chunks
+/******/b.u=e=>e+"."+{3:"f2ee02203c0deb5cd4f0",408:"3a0bdc8a2f35e3cf9c89",418:"42f2d71c93b48de2aa3f",422:"06662d0f882571a83b57",850:"60bb27e33f4981152809",863:"dfd0d2e03cbb56f0afc6",1104:"9decec9fd404eb153df4",1168:"3f37a859af8a92382c51",1190:"d20e951291ce6befa8c1",1291:"9e834e0a22d7d970397b",1351:"a028c2133c021e0cc1a8",2029:"51df31834b27ad0d6192",2530:"bd34c8e1d9067dae05a9",2659:"35811d307c0dbf68139d",2810:"95c04b664c4adcbed282",2911:"d7f0d015d1e87532ccb9",3011:"2d9207c27597abb81e96",3215:"fc587c9d28476fd5c172",3342:"42a0969eb7cf5b582a7e",3765:"28521fbbeda60db182a9",3927:"bc32cff848dfd21c82f5",4157:"976a3cee7933da9b2ec3",4567:"118f47042a0e2cd2ebb8",4592:"33acaefbb922b90e381a",4758:"a2607b05725b8ccf31f8",4937:"27cd533fc08d4302f99e",4955:"61c7e9cc6e74dd72b095",5016:"54e119bb4dfe21e27ce7",5099:"07466ff7ace44504c5cc",5139:"e7e839055e6f4c2da3f0",5315:"fbb3544fc3f95bda029e",5330:"175f285265030619d501",5558:"9e08687daeb4925d5527",5626:"b932b9c3b49eb9f60a0a",6512:"1eda788c2f3840fca3d2",6577:"8681e4eeafa9052bca5b",7093:"5544c5285be47d305974",7591:"3e6e8e04dad92aa86376",7733:"08039ac45ae0ce14e3a9",7780:"2d847f372c040e37f7c6",7847:"dc848f51d4c273af9706",7861:"cc97dbe836dc636b1981",8027:"a4bc110b714e8f2e8ff7",8243:"d199bb4444234422b4d3",8321:"b80059912ff1dabc23d2",8468:"0967a44e41b76b09863a",8540:"48f6ebee207192d495b2",8766:"ae79a1a2502675dabdc9",8871:"588b4e52b6aa1ffa064c",9157:"8b6af47f4e37194bf508",9159:"7e7bf8efd02e458ee6f8",9179:"8d9667d0bd4cb416d295",9402:"5e55c837503ab49e451a",9439:"197ec3a95db699983b84",9457:"6ca78119ad31d5e02efa",9688:"cca703e84520ed7426ec",9785:"07d00fefae345d0a7606",9879:"45c663d17debea6e9cef"}[e]+".js"
+/******/,
+/******/ // This function allow to reference async chunks
+/******/b.miniCssF=e=>e+"."+{418:"5465251b50207eb1eed1",422:"f188e254a2660d1177fe",850:"5ec79761a07a7e3b96b6",863:"5465251b50207eb1eed1",1104:"5465251b50207eb1eed1",1168:"5e8daa1b445df2dce8d7",1190:"5465251b50207eb1eed1",1291:"95cb867a6be041629e1e",2029:"4db8cf9fc1b970e7e5d3",2530:"20986e70ba97aa8e1cb4",2659:"5e8daa1b445df2dce8d7",2810:"3def85ce30ebf6c47dca",2911:"5e8daa1b445df2dce8d7",3215:"5859d378244408607d90",3342:"fb7ed7b4439531ca84ed",3765:"5e8daa1b445df2dce8d7",4592:"cef816437dda86a79287",4937:"446d0d64923fe1af286b",4955:"20986e70ba97aa8e1cb4",5016:"5465251b50207eb1eed1",5099:"48836b7609c89822034b",5139:"5465251b50207eb1eed1",5315:"1b73c27656dd4600438d",5558:"ebcd3a96cd156dffd645",5626:"5465251b50207eb1eed1",6577:"6f4f4da0e282282edc8a",7591:"5465251b50207eb1eed1",7733:"bb0658d32067bba23a94",7780:"5465251b50207eb1eed1",7847:"5465251b50207eb1eed1",8243:"c3bbecd385da5fc75ded",8468:"5465251b50207eb1eed1",8540:"20986e70ba97aa8e1cb4",8766:"e0fe0bd79052b3a182e0",9179:"5e8daa1b445df2dce8d7",9439:"ce62b1100c17173a64e7",9457:"5465251b50207eb1eed1"}[e]+".css"
+/******/,
+/******/b.o=(e,a)=>Object.prototype.hasOwnProperty.call(e,a)
+/******/,
+/******/
+/******/ /* webpack/runtime/load script */
+/******/d={},t="imc2026:",
+/******/ // loadScript function to load a script via script tag
+/******/b.l=(e,a,f,r)=>{
+/******/if(d[e])d[e].push(a);else{
+/******/var c,n;
+/******/if(void 0!==f)
+/******/for(
+/******/var o=document.getElementsByTagName("script"),i=0;i<o.length;i++){
+/******/var l=o[i];
+/******/if(l.getAttribute("src")==e||l.getAttribute("data-webpack")==t+f){c=l;break}
+/******/}
+/******/
+/******/c||(
+/******/n=!0,
+/******/
+/******/(
+/******/c=document.createElement("script")).charset="utf-8",
+/******/c.timeout=120,
+/******/b.nc&&
+/******/c.setAttribute("nonce",b.nc)
+/******/,c.setAttribute("data-webpack",t+f),
+/******/
+/******/c.src=e)
+/******/,d[e]=[a];
+/******/var u=(a,f)=>{
+/******/ // avoid mem leaks in IE.
+/******/c.onerror=c.onload=null,
+/******/clearTimeout(s);
+/******/var t=d[e];
+/******/
+/******/if(delete d[e],
+/******/c.parentNode&&c.parentNode.removeChild(c),
+/******/t&&t.forEach((e=>e(f))),a)return a(f);
+/******/}
+/******/,s=setTimeout(u.bind(null,void 0,{type:"timeout",target:c}),12e4);
+/******/c.onerror=u.bind(null,c.onerror),
+/******/c.onload=u.bind(null,c.onload),
+/******/n&&document.head.appendChild(c)}}
+/******/,
+/******/ // define __esModule on exports
+/******/b.r=e=>{
+/******/"undefined"!=typeof Symbol&&Symbol.toStringTag&&
+/******/Object.defineProperty(e,Symbol.toStringTag,{value:"Module"})
+/******/,Object.defineProperty(e,"__esModule",{value:!0})}
+/******/,
+/******/b.p="/",
+/******/
+/******/ /* webpack/runtime/css loading */
+/******/(()=>{
+/******/if("undefined"!=typeof document){
+/******/var e=e=>new Promise(((a,f)=>{
+/******/var d=b.miniCssF(e),t=b.p+d;
+/******/
+/******/if(((e,a)=>{
+/******/for(
+/******/var f=document.getElementsByTagName("link"),d=0;d<f.length;d++){
+/******/var t=(c=f[d]).getAttribute("data-href")||c.getAttribute("href");
+/******/
+/******/if("stylesheet"===c.rel&&(t===e||t===a))return c;
+/******/}
+/******/var r=document.getElementsByTagName("style");
+/******/for(d=0;d<r.length;d++){
+/******/var c;
+/******/
+/******/if((t=(c=r[d]).getAttribute("data-href"))===e||t===a)return c;
+/******/}
+/******/})(d,t))return a();
+/******/((e,a,f,d,t)=>{
+/******/var r=document.createElement("link");
+/******/
+/******/
+/******/r.rel="stylesheet",
+/******/r.type="text/css",
+/******/b.nc&&(
+/******/r.nonce=b.nc)
+/******/,
+/******/r.onerror=r.onload=f=>{
+/******/if(
+/******/ // avoid mem leaks.
+/******/r.onerror=r.onload=null,"load"===f.type)
+/******/d();
+/******/else{
+/******/var c=f&&f.type,b=f&&f.target&&f.target.href||a,n=new Error("Loading CSS chunk "+e+" failed.\n("+c+": "+b+")");
+/******/
+/******/n.name="ChunkLoadError",
+/******/n.code="CSS_CHUNK_LOAD_FAILED",
+/******/n.type=c,
+/******/n.request=b,
+/******/r.parentNode&&r.parentNode.removeChild(r)
+/******/,t(n)}
+/******/},
+/******/r.href=a,
+/******/
+/******/
+/******/f?
+/******/f.parentNode.insertBefore(r,f.nextSibling):
+/******/document.head.appendChild(r);
+/******/})(e,t,null,a,f)}
+/******/))
+/******/,a={
+/******/9121:0
+/******/};
+/******/
+/******/
+/******/b.f.miniCss=(f,d)=>{
+/******/a[f]?d.push(a[f]):0!==a[f]&&{418:1,422:1,850:1,863:1,1104:1,1168:1,1190:1,1291:1,2029:1,2530:1,2659:1,2810:1,2911:1,3215:1,3342:1,3765:1,4592:1,4937:1,4955:1,5016:1,5099:1,5139:1,5315:1,5558:1,5626:1,6577:1,7591:1,7733:1,7780:1,7847:1,8243:1,8468:1,8540:1,8766:1,9179:1,9439:1,9457:1}[f]&&
+/******/d.push(a[f]=e(f).then((()=>{
+/******/a[f]=0;
+/******/}),(e=>{
+/******/
+/******/throw delete a[f],e;
+/******/})))
+/******/}}})
+/******/
+/******/ // no hmr
+/******/
+/******/ // no prefetching
+/******/
+/******/ // no preloaded
+/******/(),
+/******/
+/******/ /* webpack/runtime/jsonp chunk loading */
+/******/(()=>{
+/******/ // no baseURI
+/******/
+/******/ // object to store loaded and loading chunks
+/******/ // undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ // [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/var e={
+/******/9121:0
+/******/};
+/******/
+/******/b.f.j=(a,f)=>{
+/******/ // JSONP chunk loading for javascript
+/******/var d=b.o(e,a)?e[a]:void 0;
+/******/if(0!==d)// 0 means "already installed".
+/******/
+/******/ // a Promise means "currently loading".
+/******/if(d)
+/******/f.push(d[2]);
+/******/else
+/******/if(9121!=a){
+/******/ // setup Promise in chunk cache
+/******/var t=new Promise(((f,t)=>d=e[a]=[f,t]));
+/******/f.push(d[2]=t);
+/******/
+/******/ // start chunk loading
+/******/var r=b.p+b.u(a),c=new Error;
+/******/ // create error before stack unwound to get useful stacktrace later
+/******/
+/******/b.l(r,(f=>{
+/******/if(b.o(e,a)&&(
+/******/
+/******/0!==(d=e[a])&&(e[a]=void 0)
+/******/,d)){
+/******/var t=f&&("load"===f.type?"missing":f.type),r=f&&f.target&&f.target.src;
+/******/
+/******/c.message="Loading chunk "+a+" failed.\n("+t+": "+r+")",
+/******/c.name="ChunkLoadError",
+/******/c.type=t,
+/******/c.request=r,
+/******/d[1](c)}
+/******/
+/******/}),"chunk-"+a,a)}else e[a]=0;
+/******/
+/******/
+/******/},
+/******/
+/******/ // no prefetching
+/******/
+/******/ // no preloaded
+/******/
+/******/ // no HMR
+/******/
+/******/ // no HMR manifest
+/******/
+/******/b.O.j=a=>0===e[a]
+/******/
+/******/ // install a JSONP callback for chunk loading
+/******/;var a=(a,f)=>{
+/******/var d,t,[r,c,n]=f,o=0;
+/******/ // add "moreModules" to the modules object,
+/******/ // then flag all "chunkIds" as loaded and fire callback
+/******/
+/******/if(r.some((a=>0!==e[a]))){
+/******/for(d in c)
+/******/b.o(c,d)&&(
+/******/b.m[d]=c[d])
+/******/;
+/******/if(n)var i=n(b);
+/******/}
+/******/
+/******/for(a&&a(f);o<r.length;o++)
+/******/t=r[o],
+/******/b.o(e,t)&&e[t]&&
+/******/e[t][0]()
+/******/,e[t]=0;
+/******/return b.O(i);
+/******/}
+/******/
+/******/,f=self.webpackChunkimc2026=self.webpackChunkimc2026||[];
+/******/f.forEach(a.bind(null,0)),
+/******/f.push=a.bind(null,f.push.bind(f))})
+/******/()})
+/******/
+/************************************************************************/
+/******/
+/******/
+/******/();
--- src/pages/location/travel/index.js
+++ src/pages/location/travel/index.js
@@ -7,99 +7,94 @@ import { Link } from 'react-router-dom';
 const Travel = () => {
   return (
     <PageContain title="Travel Information">
-      <h3>How to travel to Barcelonnette</h3>
-      <div className="ps-md-3 md-5">
-        <p>
-          The location of IMC 2026 will be the town of Barcelonnette, situated in the Ubaye Valley in the department of Alpes-de-Haute-Provence, southeastern France.
-          Barcelonnette is a beautiful mountain town — but it is relatively remote and requires some planning to reach.
-        </p>
+      <div className="ps-md-3 mb-5">
+
+        <h3>How to reach Barcelonnette</h3>
+        <p>Barcelonnette is located in the Ubaye Valley, in the Alpes-de-Haute-Provence, in southeastern France. As there is no railway station in Barcelonnette itself, most public-transport journeys finish with a bus, taxi, or rental car from Gap or Marseille.</p>
+
+        <div className="alert alert-success fw-bolder">
+          In the context of climate change and the necessity to reduce fossil-fuel use, we strongly encourage participants to choose lower-carbon travel options whenever possible. Please consider travelling by train and/or carpooling with other participants, especially for the final part of the journey to Barcelonnette.
+        </div>
+
 
         <p>The address of the LOC venue is:</p>
         <p><strong>Pôle d'accueil universitaire Séolane</strong><br />
-          20 rue du Colonel Charles Péguy<br />
+          Quartier du 11è BCA, Chemin des Casernes<br />
           04400 Barcelonnette<br />
           France
+          <br /><br />
+          44.3900, 6.6423  /  44°23'24"N, 6°38'32"E
         </p>
+ 
 
-        <p><strong>Latitude:</strong> 44.389853 &nbsp; / &nbsp; <strong>Longitude:</strong> 6.642819</p>
-      </div> 
+        <h4>By plane</h4>
+        <p>The most convenient airport is usually <a href="https://www.marseille-airport.com/" target="_blank" rel="noopener noreferrer">Marseille Provence Airport (MRS)</a>. From there, you can either rent a car and drive to Barcelonnette, or continue by public transport via Marseille Saint-Charles and the regional ZOU! bus network.</p>
 
-
-      <h4 className="mt-4">By Airplane</h4>
-      <div className="ps-md-3">
-        <p className="mb-0">The most convenient airports are:</p>
+        Possible airport options:
         <ul>
-          <li><strong>Marseille Provence Airport (MRS)</strong></li>
-          <li><strong>Nice Côte d’Azur Airport (NCE)</strong></li>
+          <li><a href="https://www.marseille-airport.com/" target="_blank" rel="noopener noreferrer">Marseille Provence Airport (MRS)</a>: the most practical airport for reaching Barcelonnette. By car, the journey takes about 3 hours (~230 km) via the A51 motorway (toll highway).
+          </li>
+          <li><a href="https://www.lyonaeroports.com/" target="_blank" rel="noopener noreferrer">Lyon Saint-Exupéry Airport (LYS)</a>: convenient for many international flights. From the airport's TGV station, or after transferring to Lyon Part-Dieu station, you can take a regional train to Gap, then continue by bus, taxi, or rental car.
+          </li>
+          <li>Paris airports (<a href="https://www.parisaeroport.fr/en/passengers/charles-de-gaulle-airport" target="_blank" rel="noopener noreferrer">CDG</a> / <a href="https://www.parisaeroport.fr/en/passengers/orly-airport" target="_blank" rel="noopener noreferrer">ORY</a>): useful for long-haul international flights. From CDG or Orly, go to Paris <a href="https://www.garesetconnexions.sncf/en/stations-services/paris-gare-lyon" target="_blank" rel="noopener noreferrer">Gare de Lyon</a> and take a train to Gap, then continue by bus, taxi, or rental car.
+          </li>
+          <li>From <a href="https://www.marseille-airport.com/" target="_blank" rel="noopener noreferrer">Marseille Provence Airport</a>, regular <a href="https://www.marseille-airport.com/access-car-parks/access/bus/marseille-st-charles-station" target="_blank" rel="noopener noreferrer">airport shuttles</a> connect the airport to Marseille Saint-Charles station in about 25-50 minutes depending on traffic.
+          </li>
+          <li>
+            From Marseille Saint-Charles, take <a href="https://zou.maregionsud.fr/en/" target="_blank" rel="noopener noreferrer">ZOU!</a> Express line 68 toward Digne-les-Bains / Barcelonnette. Some connections may instead require line 69 to Gap, followed by line 535 to Barcelonnette. The journey takes between 5 and 6 hours and costs around €25 (as of June 2026).
+          </li>
         </ul>
 
-        <p>
-          From <strong>Marseille Airport</strong>, the drive to Barcelonnette takes approximately <strong>2h30 to 3h</strong> (around 160 km).
-          <br />
-          From <strong>Nice Airport</strong>, the drive takes approximately <strong>3h30 to 4h</strong> depending on traffic and mountain road conditions.
-          <br />
-          Car rental is strongly recommended from either airport, as public transport connections to Barcelonnette are limited and time-consuming.
-        </p>
-      </div>
 
-      <h4 className="mt-4">By Train</h4>
-      <div className="ps-md-3">
-        <p className="mb-0">
-          There is no train station in Barcelonnette. The closest main railway stations are:
-        </p>
+        <h4>By train</h4>
+        <p>The nearest practical railway station is Gap Gare SNCF. From Gap, Barcelonnette can be reached by bus line 535 (around 1 hour 30 minutes — although we do not yet know if this option will still be available in September), taxi, or rental car.</p>
 
+        <p><b>Important:</b> the earlier you buy your train tickets, the cheaper they usually are.</p>
+
+        <p><a href="https://www.sncf-connect.com/en-en" target="_blank" rel="noopener noreferrer">Suggested train routes:</a></p>
         <ul>
-          <li><strong>Aix-en-Provence TGV</strong></li>
-          <li><strong>Marseille Saint-Charles</strong></li>
-          <li><strong>Gap</strong> (regional trains)</li>
+          <li>From Paris: take a daytime train from Paris Gare de Lyon, usually with a connection in Valence-Ville or Grenoble. Duration: between 5h40 and 8h30. Prices from around €60.</li>
+          <li>From Marseille: take a regional TER train from Marseille Saint-Charles to Gap Gare SNCF. Duration: between 3h15 and 5h50. Prices from around €34.</li>
+          <li>From Lyon: take a regional train from Lyon Part-Dieu to Gap Gare SNCF, usually with a connection in Grenoble or Valence. Duration: between 4h30 and 5h45. Prices vary; check <a href="https://www.sncf-connect.com/en-en" target="_blank" rel="noopener noreferrer">SNCF Connect</a> for the latest prices.</li>
+          <li>The bus journey on line 535 from Gap Gare SNCF to Barcelonnette takes around 1 hour 25 minutes. The bus leaves from Gap Gare SNCF / Quai 3 and arrives at Place Aimé Gassier in Barcelonnette, about a 20-minute walk from the conference venue.</li>
         </ul>
 
-        <p>
-          From Paris, a TGV train to Aix-en-Provence TGV takes about <strong>3 hours</strong>. From there, the onward journey by bus to Barcelonnette takes approximately <strong>3h30 to 4h</strong>.
-          <br />
-          Alternatively, you may travel via <strong>Gap</strong>. From Gap, Barcelonnette is about <strong>1h30 by car</strong> or regional bus.
-          <br />
-          Overall travel time from Paris to Barcelonnette by train and bus is typically <strong>6h30 to 8h</strong>, depending on connections.
-        </p>
-      </div>
 
-      <h4 className="mt-4">By Car</h4>
-      <div className="ps-md-3">
-        <p className="mb-0">
-          Driving is often the most practical option.
-        </p>
-
+        <h4>By car</h4>
+        <p>Approximate driving times to Barcelonnette:</p>
         <ul>
-          <li>From Marseille / Aix-en-Provence: approximately <strong>2h30-3h</strong></li>
-          <li>From Grenoble: approximately <strong>2h30-3h</strong></li>
-          <li>From Nice: approximately <strong>3h30-4h</strong></li>
-          <li>From Gap: approximately <strong>1h30</strong></li>
+          <li><a href="https://www.google.fr/maps/dir/Marseille+Provence+Airport,+rd+20,+13700+Marignane/Barcelonnette,+04400/@43.9674956,5.2732107,207722m/data=!3m1!1e3!4m14!4m13!1m5!1m1!1s0x12c9e5fde9ee1837:0xe273ff8b8f69ff2e!2m2!1d5.2144565!2d43.4383502!1m5!1m1!1s0x12cc91992a2e9357:0xbcf809d3270f6cbe!2m2!1d6.6503651!2d44.3857245!3e0?entry=ttu&g_ep=EgoyMDI2MDYxNi4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer">From Marseille</a>: about 3 hours / 210 km</li>
+          <li><a href="https://www.google.fr/maps/dir/Lyon+Arpt,+A%C3%A9roport+Lyon+Saint-Exup%C3%A9ry,+69125+Colombier-Saugnieu/Barcelonnette,+04400/@45.0532433,5.1879125,203888m/data=!3m2!1e3!4b1!4m14!4m13!1m5!1m1!1s0x47f4c92b009f84cd:0x86dc510cc9b255f0!2m2!1d5.0887768!2d45.7234181!1m5!1m1!1s0x12cc91992a2e9357:0xbcf809d3270f6cbe!2m2!1d6.6503651!2d44.3857245!3e0?entry=ttu&g_ep=EgoyMDI2MDYxNi4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer">From Lyon</a>: about 4 hours / 280 km</li>
+          <li><a href="https://www.google.fr/maps/dir/A%C3%A9roport+de+Paris-Charles+de+Gaulle,+95700+Roissy-en-France/Barcelonnette,+04400/@46.6731437,1.6063124,792127m/data=!3m2!1e3!4b1!4m14!4m13!1m5!1m1!1s0x47e63e038e4ccf5b:0x42be0982f5ba62c!2m2!1d2.5507855!2d49.0078827!1m5!1m1!1s0x12cc91992a2e9357:0xbcf809d3270f6cbe!2m2!1d6.6503651!2d44.3857245!3e0?entry=ttu&g_ep=EgoyMDI2MDYxNi4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer">From Paris</a>: about 8 hours / 740 km</li>
+          <li><a href="https://www.google.fr/maps/dir/Gap,+05000/Barcelonnette,+04400/@44.4735538,6.1900986,51486m/data=!3m2!1e3!4b1!4m14!4m13!1m5!1m1!1s0x12cb3f78d8a213d5:0x40819a5fd97a760!2m2!1d6.079758!2d44.559638!1m5!1m1!1s0x12cc91992a2e9357:0xbcf809d3270f6cbe!2m2!1d6.6503651!2d44.3857245!3e0?entry=ttu&g_ep=EgoyMDI2MDYxNi4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer">From Gap</a>: about 1 hour 15 minutes / 72 km</li>
         </ul>
+        <p>Routes from Marseille, Lyon, and Paris include toll motorways. From Gap, the route is by mountain road.</p>
+        <p>From the south, the usual route is via Aix-en-Provence, then the A51 toll motorway north toward Tallard/Gap, followed by the D900 into the Ubaye Valley toward Barcelonnette.</p>
+
+
+
+
+
+        <h4 className="mt-4">Parking</h4>
+        <div className="ps-md-3">
+          <p>You can park for free in front of the venue. </p>
+        </div>
+
+
+        <GoogleMap />
 
         <p>
-          Please note that access roads are mountain roads (mainly the D900). Travel times may vary depending on traffic and weather conditions. Outside the summer season, certain mountain passes near the Italian border may be subject to restrictions.
+          If you require assistance with travel planning, the Local Organising Committee (LOC) will be happy to help. Please contact us via the {' '}
+          <Link
+            aria-label="Contact"
+            className="fw-bolder"
+            to="/contact"
+            title="Contact"
+          >
+            contact page
+          </Link>.
         </p>
       </div>
-
-      <h4 className="mt-4">Parking</h4>
-      <div className="ps-md-3">
-        <p> You can park for free in front of the hostel. </p>
-      </div>
-
-
-      <GoogleMap />
-
-      <p>
-        If you require assistance with travel planning, the Local Organising Committee (LOC) will be happy to help. Please contact us via the {' '}
-        <Link
-          aria-label="Contact"
-          className="fw-bolder"
-          to="/contact"
-          title="Contact"
-        >
-          contact page
-        </Link>.
-      </p>
 
     </PageContain >
   );
